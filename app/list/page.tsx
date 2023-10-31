9fac2bcb43bddb44c2fe25814b5f76b883ed656b
Mah <mahtabvariyani@gmail.com>
2023-10-31 14:21:33 +0100

    New test

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -13,6 +13,14 @@ const Page = () => {
     console.log(searchValue)
   };
 
+  const getResultColor = () => {
+    if (result === "This Code is Valid") {
+      return "text-green-800"; // Valid code (green text)
+    } else {
+      return "text-red-800"; // Invalid code (red text)
+    }
+  };
+
   return (
     <div style={{ height: "100vh" }}>
       <form className="m-9" onSubmit={handleSubmit}>
@@ -34,7 +42,7 @@ const Page = () => {
         </div>
       </form>
       {result && (
-        <div className="text-green-800 p-4 mt-4">
+        <div  className={`${getResultColor()} p-4 mt-4`}>
           <p className="ml-7 mr-8 text-bold">{`${result}`}</p>
         </div>
       )}
